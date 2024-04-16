bd13c683d9e06f7009a8d097d0c2b4bc2ad0ace3
Dileep Reddy <dileepreddyp7@gmail.com>
2024-04-15 20:17:07 -0400

    state and county files created

---
+++ README.md
@@ -0,0 +1,20 @@
+### State-wise and county-wise precinct maps
+
+This repository contains the data from NYT Upshot's [Presidential precinct data for the 2020 general election](https://github.com/TheUpshot/presidential-precinct-map-2020) split into individual GeoJSON files by state and individual county(-equivalent region), as well as the Clojure file I used to split the data.
+
+Each state/county GeoJSON file contains the precinct shapefiles for that geographic region.
+
+The MIT license for NYT data is in the `NYT_LICENSE` file. (The NYT data itself is not included)
+
+The files are in the `statewise` directory with the following structure
+
+statewise/
+├── {STATE CODE}/
+│   ├── {STATE CODE}.geojson
+│   └── js/
+│       ├── {COUNTY NAME}.geojson
+│       ...
+|       
+...
+
+Note: I have not verified that I have properly split the data for all 3000+ counties.
--- parse_upshot_data.clj
+++ parse_upshot_data.clj
@@ -10,7 +10,7 @@
   (str "./statewise/" state-code "/" state-code ".geojson"))
 (defn county-geojson-filepath [state-code county-name]
   (str "./statewise/" state-code "/counties/" county-name ".geojson"))
- 
+
 (comment
   ;;; run `jq -c '.features' ./dataset/precincts-with-results.geojson > ./dataset/features-only.geojson`
   ;;; to remove only the features array from the giant geojson 
@@ -23,26 +23,28 @@
         county-fips (-> row (nth 4) string/trim)
         county-name (-> row (nth 2) string/trim (string/replace #" " "_"))]
     (if (get fips-map state-fips)
-      (assoc-in fips-map [state-fips :counties county-fips] {:name county-name})
+      (assoc-in fips-map [state-fips :counties county-fips] {:name county-name
+                                                             :features []})
       (assoc fips-map state-fips {:name (-> row (nth 5) string/trim)
-                                  :counties {county-fips {:name county-name}}}))))
+                                  :features []
+                                  :counties {county-fips {:name county-name
+                                                          :features []}}}))))
 
 (defn make-fips-map [filepath]
   (with-open [r (io/reader filepath)]
-  (->> (csv/read-csv r :separator \tab)
-       (rest)
-       (reduce convert-fips-to-map {}))))
+    (->> (csv/read-csv r :separator \tab)
+         (rest)
+         (reduce convert-fips-to-map {}))))
 
 (def fips-map (make-fips-map fips-table-filepath))
+(def fips-keys (keys fips-map))
 
-(get-in fips-map ["36" :counties "36001"])
-
-(defn make-directories []
-  (->> (keys fips-map)
+(defn make-directories [fips-map]
+  (->> fips-keys
        (map #(get-in fips-map [% :name]))
        (map #(.mkdirs (io/file (str "./statewise/" % "/counties"))))))
 
-(make-directories)
+(make-directories fips-map)
 
 (defn geojson-format [region-name feature-array]
   {:type "FeatureCollection"
@@ -53,20 +55,39 @@
 
 (def get-county-fips #(subs (-> % :properties :GEOID) 0 5))
 
-(def get-state-features #(get-in %1 [%2 :features] false))
+#_(def get-state-features #(get-in %1 [%2 :features]))
 
-(def get-county-features #(get-in %1 [%2 :counties %3 :features] false))
+#_(def get-county-features #(get-in %1 [%2 :counties %3 :features]))
 
-#_(defonce counter (atom 0))
-#_(def print-counter (when (zero? (rem @counter 1000)) (println @counter)))
+(defonce counter (atom 0))
 
-#_(defn split-by-state [filename batchsize]
-    (with-open [r (io/reader filename)]
-      (doseq [batch (partition-all batchsize (json/parse-stream r keyword))]
-        (swap! counter inc)
-        (println @counter)
-        (doseq [feature batch]
-          (state-feature-vec-conj feature)))))
+(defn copy-features-to-map [current-state-fips state-map feature]
+  (let [state-fips (get-state-fips feature)
+        county-fips (get-county-fips feature)]
+    (if (= state-fips current-state-fips)
+      (update-in (update state-map :features conj feature) [:counties county-fips :features] conj feature)
+      state-map)))
+
+(defn save-map-to-files [state-map]
+  (let [state-code (:name state-map)]
+    (with-open [w (io/writer (state-geojson-filepath state-code))]
+      (json/generate-stream (geojson-format state-code (:features state-map)) w))
+    (doseq [county (vals (:counties state-map))] 
+      (let [county-name (:name county)]
+        (with-open [w (io/writer (county-geojson-filepath state-code county-name))]
+          (json/generate-stream (geojson-format county-name (:features county)) w))))))
+
+(defn split-by-state [filename state-fips]
+  (with-open [r (io/reader filename)]
+    (swap! counter inc)
+    (println @counter ":" (get-in fips-map [state-fips :name]))
+    (as-> (json/parse-stream r keyword) $
+      (reduce (partial copy-features-to-map state-fips) (get fips-map state-fips) $)
+      (save-map-to-files $))))
+
+(doseq [state-fips fips-keys]
+  (split-by-state features-only-filepath state-fips))
+
 
 
 
